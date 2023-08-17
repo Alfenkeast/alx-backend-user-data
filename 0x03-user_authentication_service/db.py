b339b8a9a82a8d1c299af88d9f26e74aed93df3a
Patience <alfredpatience03@gmail.com>
2023-08-17 17:51:38 +0100

    user_authentication

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,6 @@
 import logging
 from typing import Dict
 
-from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
